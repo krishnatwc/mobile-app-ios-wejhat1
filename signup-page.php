bf2550e1f8b76a9017696adb9bb9a665181a8493
krishnatwc <33319559+krishnatwc@users.noreply.github.com>
2018-09-22 12:13:44 +0530

    Add files via upload
    update

---
+++ signup-page.php
@@ -0,0 +1,63 @@
+<div data-page="signup-page" class="page cached">
+  <div class="navbar login-screen-navbar">
+        <div class="navbar-inner">
+            <div class="left"><a href="#!/login-page.html" class="link back backLink"><i class="material-icons">arrow_back</i></a>
+                <div class="item-inner">
+                    <div class="item-title-row">
+                        <div class="item-title search-resultsPageNavbarTitle">Sign Up</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+    
+   <div class="page-content login-screen-content">
+    <div class="ContentdivBck">
+   <!-- <div class="login-screen-title">Adivaha</div>-->
+    <form>
+      <div class="list-block inputs-list">
+        <ul>
+          <li class="item-content">
+            <div class="item-inner"> 
+              <div class="item-title label">Username</div>
+              <div class="item-input">
+                <input type="text" name="username" placeholder="Your username"/>
+              </div>
+            </div>
+          </li>
+          <li class="item-content">
+            <div class="item-inner"> 
+              <div class="item-title label">Email </div>
+              <div class="item-input">
+                <input type="text" name="password" placeholder="Your password"/>
+              </div>
+            </div>
+          </li>
+		  <li class="item-content">
+            <div class="item-inner"> 
+              <div class="item-title label">Phone</div>
+              <div class="item-input">
+                <input type="text" name="password" placeholder="Your password"/>
+              </div>
+            </div>
+          </li>
+		  <li class="item-content">
+            <div class="item-inner"> 
+              <div class="item-title label">Password</div>
+              <div class="item-input">
+                <input type="password" name="password" placeholder="Your password"/>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="content-block"><a href="#" class="button button-big">Sign In</a></div>
+	  <div class="content-block"><a href="#" class="signupLink">sign up</a></div>
+      <div class="content-block">
+        <div class="list-block-label">Some text about login information.<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      </div>
+    </form>
+	 </div>
+  </div>
+</div>
+
