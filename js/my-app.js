bb9ff1d6124d8c4c5a34f922880ec042acea3d8e
krishnatwc <33319559+krishnatwc@users.noreply.github.com>
2018-09-25 13:04:09 +0530

    Add files via upload
    update

--- js/my-app.js
+++ js/my-app.js
@@ -46,8 +46,65 @@ var mainView = myApp.addView('.view-main', {
 
 
 var RequestURL ='https://www.adivaha.com/demo/MobAppRequest';
-$$(document).on('pageInit',function(e){
 
+
+myApp.onPageInit('index', function (page) {
+	
+var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];	
+
+var strDate =new Date();	
+var enrDate =new Date();
+strDate.setDate(strDate.getDate() + 1);
+enrDate.setDate(enrDate.getDate() + 2);
+
+
+//var sMonth =start.getMonth() < 12 ? start.getMonth() + 1 : 1;
+//var eMonth =end.getMonth() < 12 ? start.getMonth() + 1 : 1;
+
+var checkIn = strDate.getFullYear()+"-"+ (strDate.getMonth()+1) + "-" + strDate.getDate();
+var checkOut = enrDate.getFullYear()+"-"+ (enrDate.getMonth()+1) + "-" + enrDate.getDate();
+
+
+var startDate_txt = weekday[strDate.getDay()]+', '+strDate.getDate()+' '+monthNames[(strDate.getMonth()+1)]+' '+strDate.getFullYear().toString().substr(-2);
+var endDate_txt = weekday[enrDate.getDay()]+', '+enrDate.getDate()+' '+monthNames[(enrDate.getMonth()+1)]+' '+enrDate.getFullYear().toString().substr(-2);
+
+
+var htmlHotel ='<div class="history-home-page-main-left"><i class="fa fa-home"></i></div><a href="https://flight-images.adivaha.com/hotels?marker=40247&destination=New Delhi&checkIn='+checkIn+'&checkOut='+checkOut+'&adults=2&children=&language=en&currency=USD&&cityId=24077" class="link external"><div class="history-home-page-main-right"><div class="history-home-text">New Delhi</div><div class="history-home-text1">'+startDate_txt+' - '+endDate_txt+'</div><div class="history-home-text2"><i class="fa fa-user"></i> 2 Guests </div><div class="history-home-text3"><i class="fa fa-bed"></i>1 Room </div></a></div>';
+$$('#storeHotelLists').html(htmlHotel);
+
+var htmlFlight ='<div class="history-home-page-main-left">'+
+					'<i class="fa fa-plane"></i>'+
+				'</div>'+
+				'<div class="history-home-page-main-right">'+
+				  '<div class="history-recents">'+
+						'<div class="history-recents-left">'+
+						 '<a href="https://apptravelpayouts.adivaha.com/flights?marker=40247&origin_name=Delhi,%20India&origin_iata=DEL&destination_name=Goa,%20India&destination_iata=GOI&depart_date='+checkIn+'&return_date=&Flights_Return_direct=enable&with_request=true&adults=1&children=0&infants=0&trip_class=0&currency=USD&locale=en&one_way=true&ct_guests=1passenger&ct_rooms=1" class="link external"><div class="deltopatfri">'+
+							'<div class="deltopatfri1">'+
+								'<span>DEL</span> <span><i class="fa fa-arrow-right"></i></span> <span>GOI</span>'+
+								'</div>'+
+									'<div class="deltopatfri2">'+
+								'<span>'+startDate_txt+'</span>'+
+							'</div>'+
+							'</div></a>'+
+							'<a href="https://apptravelpayouts.adivaha.com/flights?marker=40247&origin_name=Delhi,%20India&origin_iata=DEL&destination_name=Mumbai,%20India&destination_iata=BOM&depart_date='+checkIn+'&return_date=&Flights_Return_direct=enable&with_request=true&adults=1&children=0&infants=0&trip_class=0&currency=USD&locale=en&one_way=true&ct_guests=1passenger&ct_rooms=1" class="link external"><div class="deltopatfri">'+
+							'<div class="deltopatfri1">'+
+								'<span>DEL</span> <span><i class="fa fa-arrow-right"></i></span> <span>BOM</span>'+
+								'</div>'+
+									'<div class="deltopatfri2">'+
+								'<span>'+startDate_txt+'</span>'+
+							'</div>'+
+							'</div></a>'+
+					'</div>'+
+					'</div>'+
+				'<div class="history-home-text2"><i class="fa fa-briefcase"></i>  Economy<span class="economyadlet">1 <i class="fa fa-male"></i> </span> </div>'+
+			   '</div>';
+$$('#storeFlightLists').html(htmlFlight);
+
+}).trigger();
+
+
+$$(document).on('pageInit',function(e){
  var page =e.detail.page;
 
 if(page.name=='search-hotels'){
@@ -313,8 +370,6 @@ if(page.name=='search-hotels'){
   $$('.findFlightResults').on('click', function(e){
 		var url ='search-flight-results.html?destination='+$$('#destination').val()+'&latitude='+$$('#latitude').val()+'&longitude='+$$('#longitude').val()+'&checkIn='+$$('#startDate').val()+'&checkOut='+$$('#endDate').val()+'&Cri_currency=USD&Cri_language=en_US&hotelType=1&rooms='+$$('#number_of_rooms').val()+'&adults='+adultsArr+'&childs='+childsArr+'&childAge=';
 	    mainView.router.loadPage(url);
-		
-		
    });
   
   
@@ -338,6 +393,7 @@ if(page.name=='search-hotels'){
       var endDateArr =endDate.split('/');
       var checkIn =startDateArr[2]+'-'+startDateArr[0]+'-'+startDateArr[1];
 	  var checkOut =endDateArr[2]+'-'+endDateArr[0]+'-'+endDateArr[1];
+
 	  var param ='marker=40247&destination='+$$('#destination').val()+'&checkIn='+checkIn+'&checkOut='+checkOut+'&adults='+adults+'&children='+childAgeArr+'&language=en&currency=USD&&cityId='+$$('#region_id').val();
      var url ='https://flight-images.adivaha.com/hotels?'+param;
      window.location.href=url;
